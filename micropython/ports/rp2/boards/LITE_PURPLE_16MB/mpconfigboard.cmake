bf63516969e282af4356f7beaf70ab2dfad73a97
sav-lab <61251456+sav-lab@users.noreply.github.com>
2023-06-18 02:13:07 +0300

    Add files via upload

---
+++ micropython/ports/rp2/boards/LITE_PURPLE_16MB/mpconfigboard.cmake
@@ -0,0 +1,1 @@
+# cmake file for Raspberry Pi RP2040 Lite Purple 16MB
---
+++ micropython/ports/rp2/boards/LITE_PURPLE_16MB/mpconfigboard.h
@@ -0,0 +1,5 @@
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "Raspberry Pi RP2040 Lite Purple 16MB"
+#define MICROPY_HW_FLASH_STORAGE_BYTES (15 * 1024 * 1024)
+
+// User LED GPIO25
